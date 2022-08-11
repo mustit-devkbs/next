b7e4ef6dc751fef3b570dee2f4402060cb812522
권봉수 <majestyb@gwonbongsuui-MacBookPro.local>
2022-08-12 01:53:26 +0900

    tsyringe docs view..

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+// import "reflect-metadata";
 import cookies from "next-cookies";
 import "../styles/globals.css";
 import type { AppContext, AppProps } from "next/app";
